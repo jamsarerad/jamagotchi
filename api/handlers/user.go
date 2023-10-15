cb7e390207426593aecdaa376e20a980829049b8
Alex <alex3whitlock@gmail.com>
2023-10-14 20:58:24 -0400

    adding database connection

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -3,57 +3,46 @@ package handlers
 import (
   "net/http"
   "encoding/json"
-  // "jamagotchi/models"
-  // "jamagotchi/db"
-  // "github.com/golang-jwt/jwt"
+  "jamagotchi/models"
+  "jamagotchi/db"
+  "github.com/golang-jwt/jwt"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
-  response := map[string]string{"message": "Connection successful"}
+  var user models.User // Assume User is a struct you've defined
+  _ = json.NewDecoder(r.Body).Decode(&user)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+  // Connect to database
+  database := db.ConnectToUsers()
 
-	json.NewEncoder(w).Encode(response)
-  // var user models.User // Assume User is a struct you've defined
-  // _ = json.NewDecoder(r.Body).Decode(&user)
-  //
-  // // Register the user in the database
-  // db.RegisterUser(user)
-  //
-  // w.Header().Set("Content-Type", "application/json")
-  // json.NewEncoder(w).Encode(map[string]string{"message": "User registered successfully"})
-  //
-  // // Parse username and password from request body
-  // var credentials struct {
-  //   Username string `json:"username"`
-  //   Password string `json:"password"`
-  // }
-  // _ = json.NewDecoder(r.Body).Decode(&credentials)
-  //
-  // // Check username and password against database (in this case, a map)
-  // storedPassword, ok := users[credentials.Username]
-  // if !ok {
-  //   http.Error(w, "Invalid credentials", http.StatusUnauthorized)
-  //   return
-  // }
-  //
-  // // Compare hashed passwords
-  // if err := bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(credentials.Password)); err != nil {
-  //   http.Error(w, "Invalid credentials", http.StatusUnauthorized)
-  //   return
-  // }
-  //
-  // // Create a new token
-  // token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-  //   "username": credentials.Username,
-  //   })
-  //
-  // // Sign and get the complete encoded token as a string
-  // tokenString, _ := token.SignedString([]byte("your_secret_key"))
-  //
-  // // Return the token
-  // w.Header().Set("Authorization", "Bearer "+tokenString)
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(map[string]string{"message": "User registered successfully"})
+
+  // Parse username and password from request body
+  var credentials struct {
+    Username     string `json:"username"`
+    Password     string `json:"password"`
+    Email        string `json:"email"`
+    LeetCodeUser string `json:"leetcode_username"`
+  }
+  _ = json.NewDecoder(r.Body).Decode(&credentials)
+
+  // Register the user in the database
+  _, err := db.RegisterUser(database, credentials.Username, credentials.Email, credentials.Password, credentials.LeetCodeUser)
+  if err != nil {
+    http.Error(w, "Failed to register", http.StatusServiceUnavailable)
+  }
+
+  // Create a new token
+  token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+    "username": credentials.Username,
+    })
+
+  // Sign and get the complete encoded token as a string
+  tokenString, _ := token.SignedString([]byte("your_secret_key"))
+
+  // Return the token
+  w.Header().Set("Authorization", "Bearer "+tokenString)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
